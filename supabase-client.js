c626685011e7e9c4526d5e0b30ab5dec6da85839
enzolarrea <enzolarrea7@gmail.com>
2025-11-06 23:10:00 -0300

    3 Commit

--- supabase-client.js
+++ supabase-client.js
@@ -1,8 +1,8 @@
-// supabase-client.js
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-// Usa las variables globales definidas en config.js
-export const supabase = createClient(window.SUPABASE_URL, window.SUPABASE_ANON_KEY);
+export const supabase = createClient(
+    "https://pxzfneyporftbfquqllw.supabase.co", // tu URL real
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB4emZuZXlwb3JmdGJmcXVxbGx3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI0Njk3NjYsImV4cCI6MjA3ODA0NTc2Nn0.t6TlZZ6QsCToRpTXsoLk1wZF5y9Avs0cnsoCqfIRBwY"
+);
 
-// Verificación opcional
-console.log("✅ Conexión a Supabase inicializada:", window.SUPABASE_URL);
+console.log("✅ Conexión a Supabase inicializada:", supabase);
